c140017f1dcc9c439f5442c3fdefb8cd746fb7bf
Remagined-Servers <remagined.servers@gmail.com>
2025-04-27 17:43:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const discordWebhook = "https://discord.com/api/webhooks/1366060956072939581/_ahfJu2oqH2XXGN-jNHR0jM86EXW8A7LOSa4kE1s2ayBW2zCGi9-ARpXJ0uk09q1h-lF";
 
@@ -10,8 +12,8 @@ app.post('/forward', async (req, res) => {
         await axios.post(discordWebhook, { content: req.body.content });
         res.sendStatus(200);
     } catch (error) {
-        console.error(error);
-        res.sendStatus(500);
+        console.error("Error:", error.response ? error.response.data : error.message);
+        res.status(500).send("Internal Server Error");
     }
 });
 
