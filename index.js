7223868e6bd7187f5b19f532a0a41cc1088f4243
Remagined-Servers <remagined.servers@gmail.com>
2025-04-27 17:39:10 +0300

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const discordWebhook = "https://discord.com/api/webhooks/yourWebhook"; // <-- replace with YOUR webhook
+const discordWebhook = "https://discord.com/api/webhooks/1366060956072939581/_ahfJu2oqH2XXGN-jNHR0jM86EXW8A7LOSa4kE1s2ayBW2zCGi9-ARpXJ0uk09q1h-lF";
 
 app.post('/forward', async (req, res) => {
     try {
